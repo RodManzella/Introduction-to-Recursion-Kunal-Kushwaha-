b0de5a47201e44b4527faa6c072d21de99ed776d
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-03-06 13:05:23 -0300

    bs

--- RecursionProject/src/IntroductionToRecursion/BS.java
+++ RecursionProject/src/IntroductionToRecursion/BS.java
@@ -5,7 +5,7 @@ public class BS {
     public static void main(String[] args) {
         
         int [] arr = {1, 2, 3, 4, 55, 66, 78};
-        int target = 1;
+        int target = 78;
         System.out.println(search(arr, target, 0, arr.length - 1));
     }
 
