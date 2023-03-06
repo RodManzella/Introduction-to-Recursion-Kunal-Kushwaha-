0c783f1c7443969019733623c72ff7ed5b575c9c
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-03-05 22:52:35 -0300

    new commit

--- RecursionProject/src/exercises/NumbersExample.java
+++ RecursionProject/src/exercises/NumbersExample.java
@@ -3,7 +3,7 @@ public class NumbersExample {
     public static void main(String[] args) {
         // write a function that takes in a number and prints it
         // print first 5 numbers: 1 2 3 4 5
-        print1(1);
+        print1(1); 
     }
 
     static void print1(int n){
---
+++ RecursionProject/src/exercises/NumbersExampleRecursion.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class NumbersExampleRecursion {
+    
+    public static void main(String[] args) {
+        
+        print(1);
+    }
+    
+    static void print(int n){
+
+        if(n == 5){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n + 1);
+    }
+}
