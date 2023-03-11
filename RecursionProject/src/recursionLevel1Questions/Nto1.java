904203fac88eee7fc6883ffec1e6e93e205d42db
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-03-11 19:38:46 -0300

    commit

---
+++ RecursionProject/src/recursionLevel1Questions/Nto1.java
@@ -0,0 +1,28 @@
+package recursionLevel1Questions;
+
+public class Nto1{
+
+
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    static void fun(int n){
+        if(n == 0){
+            return;
+        }
+
+        System.out.println(n);
+        fun(n - 1);
+    }
+
+    static void funcReverse(int n){
+        if(n == 0){
+            return;
+        }
+
+        funcReverse(n - 1);
+        System.out.println(n);
+        
+    }
+}
